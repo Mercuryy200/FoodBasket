77e7b39c5e16db89bda1a238d09eab8693f8b71a
Rima Nafougui <Rimanafougui99@gmail.com>
2025-11-26 20:24:21 -0500

    Player Control: Added a script for the user Input and a bound for each side of the counter

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using Mono.Cecil.Cil;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private float xBound = 4;
+    public float speed = 5.0f;
+    
+
+    void Awake()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        if (playerRb == null)
+        {
+            Debug.LogError($"Rigidbody component missing on {gameObject.name}");
+        }
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+    
+    //https://docs.unity3d.com/6000.2/Documentation/ScriptReference/Mathf.Clamp.html
+    void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+        float clampedX = Mathf.Clamp(transform.position.x, -xBound, xBound);
+        transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
+    }
+}
